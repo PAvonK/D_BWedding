839a884d0b308ccc3fc9f8b8bc79e455673b21a9
Fidranth <pavk13@outlook.com>
2020-06-14 09:46:54 -0600

    Base Site initialization

---
+++ django.env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\programming_master\projects\d_bwedding\django.env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
