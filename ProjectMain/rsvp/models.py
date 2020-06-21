d42c2e8e3686526315c0d7fd72970ec5b6f5dd78
Fidranth <pavk13@outlook.com>
2020-06-21 10:45:54 -0600

    updates to rsvp app

--- ProjectMain/rsvp/models.py
+++ ProjectMain/rsvp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Rsvp(models.Model):
+    lastname = models.CharField(max_length=100)
+    firsname = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(default=timezone.now)
+    email = models.EmailField(max_length=100)
+    attending = models.TextChoices('No', 'Yes')
+    number_guests = models.CharField(max_length=2)
+    couple_message = models.TextField()
+
+
+
+
+
+class Snippet(models.Model):
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.name
--- ProjectMain/rsvp/views.py
+++ ProjectMain/rsvp/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import RsvpForm, SnippetForm
+
 
 # Create your views here.
 
 @login_required
 def rsvp(request):
-    return render(request, 'rsvp/rsvp.html')
+    
+    if request.method == "POST":
+        form = RsvpForm(request.POST)
+        if form.is_valid():
+            #form.save()
+            name = form.cleaned_data.get['name']
+            #name_last = form.cleaned_data['name_last']
+            email = form.cleaned_data['email']
+            messages.success(request, f'Thank you {name} for your RSVP!!!')
+            return redirect('location')
+            
+            print(name)
+            
+
+    else:
+        form = RsvpForm()
+    return render(request, 'rsvp/rsvp.html', {'form': form})
+
+
+def snippet_detail(request):
+
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+    form = SnippetForm()
+    return render(request, 'rsvp/rsvp.html', {'form': form})
